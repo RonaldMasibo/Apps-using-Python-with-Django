d8cedab9079f5a1469e2daffb26d4c6959e01754
MASIBO <ronaldmathews14@gmail.com>
2026-01-14 02:40:55 +0300

    Application for setting up your profile

---
+++ Profile App/ProfileProject/ProfileApp/urls.py
@@ -0,0 +1,13 @@
+
+
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.base, name='base'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('home/', views.home, name='home'),
+    path('profile/', views.profile, name='profile'),
+]
---
+++ Profile App/ProfileProject/ProfileApp/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+def register(request)
+
+
+def login(request)
+    
+
+def home(request)
+
+
+def profile(request)
+
